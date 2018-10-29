51e01ce6343fab67e278703bfbe27dbbf2512cf9
neripark <neripark@gmail.com>
2018-10-29 15:14:01 +0900

    Helloコントローラを追加、名前付きルーティングの動作を確認

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   resources :users
   get 'home/index'
+  get '/hello', to: 'application#hello', as: 'hoge'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
