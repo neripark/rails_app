89a431fc3e1443a8af51a1eff1d548c5d385152f
neripark <neripark@gmail.com>
2018-11-19 16:02:10 +0900

    フィルタとクラス変数の動作検証OK - only: 等で制限をかけないと、全フィルタがどのアクションでも実行される - 上から処理される。 - クラス変数はbefore_actionの前に実行されるので、裸では書かないほうがいい

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: [:index]
+  
+  before_action :init_var
+  before_action :require_login
+  before_action :require_admin_user, only: [:index]
+  after_action :clear_var
+  
   def index
-    render plain: "hogehoge"
+    # render plain: "hogehoge"
+    render plain: @@var_a
+  end
+
+  def index2
+    # render plain: "hogehoge"
+    render plain: @@var_a
   end
 
   def show
@@ -9,7 +20,19 @@ class UsersController < ApplicationController
 
   private  
   def require_login
+    @@var_a += "login! "
     # redirect_to login_url unless singed_in?
     # render plain: "fuga"
+  end
+  
+  def require_admin_user
+    @@var_a += "admin! "
+  end
+
+  def init_var
+    @@var_a = "start:: "
+  end
+  def clear_var
+    @@var_a = ""
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'home/index'
   get '/hello', to: 'application#hello', as: 'hoge'
+  get '/users', to: 'users#index'
+  get '/users2', to: 'users#index2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
